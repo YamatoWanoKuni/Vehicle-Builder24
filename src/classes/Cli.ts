8a8c6e008e041e24e284a08cbf8b4effdeeee588
Christian <cmcarbajal05@gmail.com>
2024-10-15 15:13:08 -0400

    added truck class with properties and constructor. added cli to call truck and motorbike once questions are answered

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -7,15 +7,12 @@ import Wheel from "./Wheel.js";
 
 // define the Cli class
 class Cli {
-  // TODO: update the vehicles property to accept Truck and Motorbike objects as well
-  // TODO: You will need to use the Union operator to define additional types for the array
   // TODO: See the AbleToTow interface for an example of how to use the Union operator
-  vehicles: (Car)[];
+  vehicles: (Car)[] | (Motorbike)[] | (Truck)[];
   selectedVehicleVin: string | undefined;
   exit: boolean = false;
 
-  // TODO: Update the constructor to accept Truck and Motorbike objects as well
-  constructor(vehicles: (Car)[]) {
+  constructor(vehicles: (Car)[] | (Motorbike)[] | (Truck)[]) {
     this.vehicles = vehicles;
   }
 
@@ -60,8 +57,7 @@ class Cli {
           type: 'list',
           name: 'vehicleType',
           message: 'Select a vehicle type',
-          // TODO: Update the choices array to include Truck and Motorbike
-          choices: ['Car'],
+          choices: ['Car','Motorbike','Truck'],
         },
       ])
       .then((answers) => {
@@ -69,7 +65,12 @@ class Cli {
           // create a car
           this.createCar();
         }
-        // TODO: add statements to create a truck or motorbike if the user selects the respective vehicle type
+        else if (answers.vehicleType === 'Motorbike') {
+          this.createMotorbike()
+        }
+        else if (answers.vehicleType === 'Truck') {
+          this.createTruck()
+        }
       });
   }
 
@@ -174,6 +175,17 @@ class Cli {
         // TODO: push the truck to the vehicles array
         // TODO: set the selectedVehicleVin to the vin of the truck
         // TODO: perform actions on the truck
+        const truck = new Truck (
+          Cli.generateVin(),
+          answers.color,
+          answers.make,
+          answers.model,
+          parseInt(answers.year),
+          parseInt(answers.weight),
+          parseInt(answers.topSpeed),
+          answers.towingCapacity,
+      )
+        
       });
   }
 
